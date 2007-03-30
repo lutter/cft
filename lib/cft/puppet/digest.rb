80cf4fcf92927526874df9e95e427ed465be07e1
David Lutterkort <dlutter@redhat.com>
2007-03-30 13:15:55 -0700

    Fix silly typo from last change

--- lib/cft/puppet/digest.rb
+++ lib/cft/puppet/digest.rb
@@ -102,6 +102,7 @@ module Cft::Puppet
             trans = transportable
             trans.each_obj do |f|
                 if f.type == :file
+                    src = f[:source]
                     next unless src
                     tgt = File::join(bpath, File::basename(src))
                     FileUtils::cp_r(src, tgt)
